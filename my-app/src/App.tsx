1360e5ba254a4d5496012ba0e597051ba2c0a22b
Kiyan Pourmaleki <kiyan.pourmaleki@mymail.champlain.edu>
2023-02-08 14:51:36 -0500

    personalized the landing page. added a few components (enter name, enter game code, create new game). they do nothing yet

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@mui/material/TextField';
+import { useAutocomplete } from '@mui/base';
+import Button from '@mui/material/Button';
 
-function App() {
+
+function LandingPage(props: any) {
+
+  const [newGame, setNewGame] = useState(false);
+
+  interface State {
+    name: string;
+    gamecode: string;
+  }
+
+  const [values, setValues] = useState<State>({
+    name: '',
+    gamecode: ''
+  });
+
+  const handleChange = 
+    (prop: keyof State) => 
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValues({...values, [prop]: event.target.value});
+        
+    };
+
+  const isNewGame = () => {
+      setNewGame(newGame);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+                
+        <TextField 
+          sx={{ m: 2 }}
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          onChange={handleChange('name')} 
+        />
+        <TextField
+          sx={{ m: 2 }}
+          id="outlined-basic"
+          label="Game Code"
+          variant="outlined"
+          onChange={handleChange('gamecode')} 
+        />
+        <Button 
+          sx={{ m: 2 }}
+          onClick={isNewGame}
+          variant="contained"
+          size="large">
+            Start New Game</Button>
+        
       </header>
     </div>
   );
 }
 
-export default App;
+export default LandingPage;
