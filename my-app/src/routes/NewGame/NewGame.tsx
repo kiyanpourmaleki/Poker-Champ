9017075307b509965797137bf46990ab67354621
Kiyan Pourmaleki <kiyan.pourmaleki@mymail.champlain.edu>
2023-03-02 22:35:49 -0500

    Navbar added with changing status. Name and game code passed to join game.

--- my-app/src/routes/NewGame/NewGame.tsx
+++ my-app/src/routes/NewGame/NewGame.tsx
@@ -5,6 +5,7 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
+import Navbar from '../../components/navbar';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -28,29 +29,9 @@ function GoBack(){
 export default function DisplayNewGame() {
     return(
         <div className="backgroundImage2">
-            <header className="New-Game-Header">
-                <Typography variant="h3" color="white">
-                    New Game!</Typography>
-                <GoBack />
-            </header>
-
-            <div className='table'>
-                <Grid container spacing={3} sx={{ flexGrow: 1 }}>
-                <Grid xs={6} xsOffset={3} md={2} mdOffset={0}>
-                    <Item>1</Item>
-                </Grid>
-                <Grid xs={4} md={2} mdOffset="auto">
-                    <Item>2</Item>
-                </Grid>
-                <Grid xs={4} xsOffset={4} md={2} mdOffset={0}>
-                    <Item>3</Item>
-                </Grid>
-                <Grid xs md={6} mdOffset={2}>
-                    <Item>4</Item>
-                </Grid>
-            </Grid>
-
-            </div>
+            <Navbar status='new game'/>
+            <GoBack />
+            <div className='table'></div>
             
         </div>
 
