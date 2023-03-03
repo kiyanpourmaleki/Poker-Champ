765ecd509a654c74e04f5007a58a8474f4a3751b
Kiyan Pourmaleki <kiyan.pourmaleki@mymail.champlain.edu>
2023-03-02 20:18:31 -0500

    commit for rebase

---
+++ my-app/src/NewPage.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Typography } from '@mui/material';
+
+function NewPage(props: any){
+    return (
+        <div id="newpage">
+            <Typography variant="h1" color="black">
+                new page!!!
+            </Typography>
+        </div>
+    );
+}
+
+export default NewPage;
