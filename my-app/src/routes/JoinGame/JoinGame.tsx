d325aef65fd59fa69cf7e9df578ab820a89ce77d
[Jack Hannan] <[jack.hannan@mymail.champlian.edu]>
2023-03-27 19:11:27 -0400

    trying to get api working

--- my-app/src/routes/JoinGame/JoinGame.tsx
+++ my-app/src/routes/JoinGame/JoinGame.tsx
@@ -1,26 +1,43 @@
 import './JoinGame.css';
-import { Typography, Button } from '@mui/material';
+import { Typography } from '@mui/material';
 import { Link, Route, Routes, useLocation} from 'react-router-dom';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Navbar from '../../components/navbar';
-import React from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import cardBack from './Card_Back.jpg'
-
-
+import { json } from 'stream/consumers';
 
 export default function DisplayNewGame(props: any) {
+    const location = useLocation();
+    const name  = location.state.playername;
+    const gamecode = location.state.gamecode;
+    var deck: any;
     
+    function createDeack() {
+        while (true) {
+            fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+                .then(res => res.json())
+                .then((deckInfo) => deck = JSON.parse(deckInfo));
+            break;
+        }
+    }
 
     const test = () => {
         alert(`Name: ${name} \nGamecode: ${gamecode}`)
     }
+    
+    console.log(deck);
+    console.log(deck)
+    const dealPlayer = () => {
+        console.log(deck);
 
-    const location = useLocation()
-    const name  = location.state.playername
-    const gamecode = location.state.gamecode
+    }
+
+    
+
     return(
-        <div className="backgroundImage2">
+        <div className="backgroundImage2" onLoad={createDeack}>
             <Navbar status='in game'/>            
             <div className='tableJ'>
                 <Typography variant='h2' color='white'>{gamecode}</Typography>
@@ -36,10 +53,15 @@ export default function DisplayNewGame(props: any) {
                 </div>
                 <div className='you'>
                     <Typography variant='h4' color='white'>{name}</Typography>
+                    <button onClick={dealPlayer}>Ready</button>
                 </div>
             </div>
         </div>
 
         
     );
+}
+
+function deckUpdate(deckInfo: any): any {
+    throw new Error('Function not implemented.');
 }
