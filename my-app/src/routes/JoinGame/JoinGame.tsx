a69484327dba43c5c3ed1eb8ca34943101910456
[Jack Hannan] <[jack.hannan@mymail.champlian.edu]>
2023-04-10 21:16:30 -0400

    got to be able to deal cards to player

--- my-app/src/routes/JoinGame/JoinGame.tsx
+++ my-app/src/routes/JoinGame/JoinGame.tsx
@@ -6,38 +6,89 @@ import Paper from '@mui/material/Paper';
 import Navbar from '../../components/navbar';
 import React, {Fragment, useEffect, useState} from 'react';
 import cardBack from './Card_Back.jpg'
-import { json } from 'stream/consumers';
+import { resolve } from 'path';
 
-export default function DisplayNewGame(props: any) {
+type Deck = {
+    success: boolean;
+    deck_id: string;
+    shuffled: boolean;
+    remaining: number;
+}
+type Card = {
+    code: string;
+    image: string;
+    images: [string, string];
+    value: string;
+    suit: string;
+}
+type Deal = {
+    success: boolean;
+    deck_id: string;
+    cards: Card[];
+    remaining: number;
+}
+
+
+export default function DisplayNewGame(props: any): JSX.Element {
+    const [cards, setCard] = useState<Card[]>([])
+
+
     const location = useLocation();
     const name  = location.state.playername;
     const gamecode = location.state.gamecode;
-    var deck: any;
     
-    function createDeack() {
-        while (true) {
-            fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-                .then(res => res.json())
-                .then((deckInfo) => deck = JSON.parse(deckInfo));
-            break;
-        }
+    function createDeck(): Promise<Deck> {
+        return new Promise<Deck>(async (resolve, reject) => {
+            const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+            if (response.ok){
+                let deckTemp: Deck = await response.json();
+                resolve(deckTemp)
+            }
+            else{reject("Didn't Work")}
+        })
     }
-
-    const test = () => {
-        alert(`Name: ${name} \nGamecode: ${gamecode}`)
+    
+    let deck = createDeck();
+    let deckID: string;
+    deck.then(info => {console.log(info);
+        deckID = info.deck_id;});
+    deck.catch(bad => {console.log(bad)
+        deckID = "none";})
+    function test() {
+        alert(`Name: ${name} \nGamecode: ${gamecode}`);
     }
     
     console.log(deck);
-    console.log(deck)
+
     const dealPlayer = () => {
-        console.log(deck);
+        console.log(deckID);
+        function dealToPlayer(): Promise<Deal> {
+            return new Promise<Deal>(async (resolve, reject) => {
+                let apiCall = "https://deckofcardsapi.com/api/deck/" + deckID + "/draw/?count=2"
+                const response = await fetch(apiCall);
+                if (response.ok){
+                    let cardsInfo: Deal = await response.json();
+                    resolve(cardsInfo)
+                }
+                else{reject("Didn't Work")}
+            })
+        }
+
+        
+        let cards = dealToPlayer();
+        
+        //console.log(cards);
+        cards.then(info => { console.log(info);
+            setCard(info.cards);});
+        cards.catch(bad => {console.log(bad);})
+
 
     }
 
     
 
     return(
-        <div className="backgroundImage2" onLoad={createDeack}>
+        <div className="backgroundImage2">
             <Navbar status='in game'/>            
             <div className='tableJ'>
                 <Typography variant='h2' color='white'>{gamecode}</Typography>
@@ -53,6 +104,11 @@ export default function DisplayNewGame(props: any) {
                 </div>
                 <div className='you'>
                     <Typography variant='h4' color='white'>{name}</Typography>
+                    {cards.map(card => (
+                        <div className='playerCards'>
+                           <img src={card.image} height={50}/>
+                        </div>
+                    ))}
                     <button onClick={dealPlayer}>Ready</button>
                 </div>
             </div>
