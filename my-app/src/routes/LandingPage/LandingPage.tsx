61c0d2f3c729b478bf33847eaa2c1c9893abd7f0
Kiyan Pourmaleki <kiyan.pourmaleki@mymail.champlain.edu>
2023-04-17 12:44:41 -0400

    new attempt at login

--- my-app/src/routes/LandingPage/LandingPage.tsx
+++ my-app/src/routes/LandingPage/LandingPage.tsx
@@ -1,13 +1,41 @@
-import React, {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './LandingPage.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Navbar from '../../components/navbar';
+import { query, collection, getDocs, where } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db, logout } from "../../firebase";
 
 export default function LandingPage(props: any) {
-  const [ name, setName] = useState("")
+
+  const [user, loading, error] = useAuthState(auth);
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const fetchUserName = async () => {
+      try {
+        const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+        const doc = await getDocs(q);
+        const data = doc.docs[0].data();
+        setName(data.name);
+      } catch (error) {
+        console.error(error);
+        alert("An error occurred while fetching user data");
+      }
+    };
+
+    if (loading) return;
+    if (!user) return navigate("/");
+    fetchUserName();
+  }, [user, error, loading, navigate]);
+
+
+  const location = useLocation()
+  const [ username, setUsername ] = location.state.username
   const [ gamecode, setGameCode] = useState("")
   const [ newGame, setNewGame ] = useState("");
   const [ existingGame, setExistingGame ] = useState("");
@@ -18,63 +46,63 @@ export default function LandingPage(props: any) {
 
   const joinGame = () => {
       setExistingGame(existingGame)
-      alert(`Name: ${name} \nGamecode: ${gamecode}`)
+      alert(`Name: ${username} \nGamecode: ${gamecode}`)
   }
 
   return (
-    <>
-      <Navbar status='home'/>
-      <div className="backgroundImage">
-        <header className="App-header">
-          <Typography variant="h4" color="black">WELCOME TO</Typography>
-          <Typography variant="h1" color="black">POKER CHAMP</Typography>
-            <TextField 
-              sx={{ m: 2 }}
-              id="outlined-basic"
-              label="Name"
-              variant="outlined"
-              name="name"
-              onChange={e => setName(e.target.value)} 
-            />
-            <TextField
-              sx={{ m: 2 }}
-              id="outlined-basic"
-              label="Game Code"
-              variant="outlined"
-              name="gamecode"
-              onChange={e => setGameCode(e.target.value)} 
-            />
-            <Button
-              disabled={!name || !gamecode} 
-              sx={{ m: 2 }}
-              onClick={joinGame}
-              variant="contained"
-              size="large"
-            >
-              <Link 
-                to='JoinGame' 
-                style={{textDecoration: 'none', color: 'white'}} 
-                state={{playername:name, gamecode:gamecode}}
+    <body>
+      <div style={{width: '100%', height: 'auto', top: '0px', bottom: '0px', left: '0' }}>
+        <Navbar status='home' user={location.state.username}/>
+        <div className="backgroundImage">
+          <header className="App-header">
+            <Typography variant="h4" color="black">WELCOME TO</Typography>
+            <Typography variant="h1" color="black">POKER CHAMP</Typography>
+              
+              {/* <TextField
+                sx={{ m: 10 }}
+                id="outlined-basic"
+                label="Game Code"
+                variant="outlined"
+                name="gamecode"
+                onChange={e => setGameCode(e.target.value)} 
+              /> */}
+              <p>Logged in as
+                <div>{name}</div>
+                <div>{user?.email}</div>
+              </p>
+
+              <Button
+                disabled={!gamecode} 
+                sx={{ m: 2 }}
+                onClick={joinGame}
+                variant="contained"
+                size="large"
               >
-                Join Game
-              </Link>
-            </Button>
-            <Typography variant="subtitle1" color="black">--------or--------</Typography>
-            <Button
-              sx={{ m: 2 }}
-              onClick={isNewGame}
-              variant="contained"
-              size="large"
-            >
-              <Link
-                to={`NewGame`}
-                style={{textDecoration: 'none', color: 'white'}}
+                <Link 
+                  to='/JoinGame' 
+                  style={{textDecoration: 'none', color: 'white'}} 
+                  state={{playername:username, gamecode:gamecode}}
+                >
+                  Join Game
+                </Link>
+              </Button>
+              <Typography variant="subtitle1" color="black">--------or--------</Typography>
+              <Button
+                sx={{ m: 2 }}
+                onClick={isNewGame}
+                variant="contained"
+                size="large"
               >
-                Start New Game
-              </Link>
-            </Button>
-        </header>
-      </div>
-    </>   
+                <Link
+                  to='/NewGame'
+                  style={{textDecoration: 'none', color: 'white'}}
+                >
+                  Start New Game
+                </Link>
+              </Button>
+          </header>
+        </div>
+      </div>   
+    </body>
   );
 }
